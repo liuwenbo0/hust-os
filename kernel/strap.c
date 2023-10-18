d4c9400e844c55342a4b8c2f56f0fadd8d14d2b4
liuwenbo0 <3097365481@qq.com>
2023-10-18 10:47:37 +0800

    my work in challenge lab2_1 is done

--- kernel/strap.c
+++ kernel/strap.c
@@ -89,7 +89,7 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
           user_vm_map((pagetable_t)current->pagetable,ROUNDDOWN(stval,PGSIZE), PGSIZE, (uint64)pa,prot_to_type(PROT_WRITE | PROT_READ, 1));
         }
         else
-           panic("In page fault illegal va.\n");
+           panic("this address is not available!");
          break;
         // }
       
